c940115878f6193c2d8a8f03b671dcbaebef902c
Gary <code@Lmnoppy.com>
2021-05-29 14:17:47 +0100

    Basic working example from tutorial

---
+++ ti/lib/pages/Loading.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+class Loading extends StatefulWidget {
+  @override
+  LoadingState createState() => LoadingState();
+}
+
+class LoadingState extends State<Loading> {
+
+  void getTime() async {
+    Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/Europe/London'));
+    Map data = jsonDecode(response.body);
+    print(data);
+
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1,3);
+    print(datetime);
+    print(offset);
+
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('loading screen'),
+    );
+  }
+}
